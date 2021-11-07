b10c028d4ef792244b970022c1bb6d0a6248bf8c
tuannnn38 <buithanhtuan9874@gmail.com>
2021-11-07 19:45:00 +0700

    some first update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: red
+    }
+  `]
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
